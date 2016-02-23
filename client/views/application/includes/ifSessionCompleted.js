5ffba4e6e046ebf0e9ac266bb752fe6a23539151
El-Mehdi-Nasad <nasad.ouarit.mehdi@gmail.com>
2016-02-23 05:22:09 +0000

    Ghange of Css design and Events of Date End

--- client/views/application/includes/ifSessionCompleted.js
+++ client/views/application/includes/ifSessionCompleted.js
@@ -1,5 +1,3 @@
-
-
 Template.ifSessionCompleted.events({
     'submit form': function(e){
 		e.preventDefault();
@@ -39,8 +37,10 @@ Template.ifSessionCompleted.events({
 	if( newtodo != "" ){	
 		var post = {
 				newtodo: newtodo,
-                                datePost: datePost,
-				etatCheckBox: false
+                                datePostDbt: datePost,
+				etatCheckBox: false,
+                                datePostFin: "",
+                                estDateFin: false
 		}
         if (Session.get("Inviter") != null ){
         var existDeja = true ;
@@ -66,14 +66,44 @@ Template.ifSessionCompleted.events({
      
       "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
+                var dateFr =  function() {
+                    // on recupere la date
+                    var date = new Date();
+                    // on construit le message
+                    var jour = date.getDate() ;
+                    if(jour < 10){
+                       jour = "0" + jour;}
+                    var message = jour + "/";   // nom du jour
+                    var mois = date.getMonth()+1 ;
+                    if(mois < 10){
+                       mois = "0" + mois;}
+                    message += mois + "/";   // mois
+                    message += date.getFullYear() + "," + " ";
+                    var heure = date.getHours();
+                    if(heure < 10){
+                       heure = "0" + heure;}
+                    message += heure + "h";
+                    var minutes = date.getMinutes();
+                    if(minutes < 10){
+                       minutes = "0" + minutes;}
+                    message += minutes + "min";
+                    var secondes = date.getSeconds();
+                    if(secondes < 10){
+                       secondes = "0" + secondes;}
+                    message += secondes + "s";
+                 return message;
+           };
         var tab = Session.get("Inviter");
         for ( elmt in tab ){
           if( tab[elmt].newtodo == this.newtodo ){
+            tab[elmt].datePostFin = dateFr(); 
               if( tab[elmt].etatCheckBox){
               tab[elmt].etatCheckBox = false;
+                               tab[elmt].estDateFin = false;
           Session.set("Inviter" , tab );}
              else{
               tab[elmt].etatCheckBox = true;
+                               tab[elmt].estDateFin = true;
           Session.set("Inviter" , tab );}
                
               }
